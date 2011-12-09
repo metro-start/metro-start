b328e8aba9830ab0f1785b4c2b815c609c3cc81e
chustar <cnnaji2003@gmail.com>
2011-12-09 12:13:01 -0800

    Moved a function down

--- metro-select/metro-select.js
+++ metro-select/metro-select.js
@@ -27,39 +27,36 @@
         $("#sel-0").click(function () {
             var select = $("#select-box");
             //if left button clicked, swipe left
-            if(select.val() != $("#sel-0")) { 
                 select.val($("#sel-0").text());
                 $("#sel-0").addClass('sel-active');
                 $("#sel-1").removeClass("sel-active");
                 $("#selector").animate({scrollLeft: 0}, "fast");
                 settings.onchange();
-            }
         });
 
         //right side button
         $("#sel-1").click(function () {
             var select = $("#select-box");
             //if the right button clicked swipe right
-            if(select.val() != $("#sel-1")) { 
                 select.val($("#sel-1").text());
                 $("#sel-0").removeClass('sel-active');
                 $("#sel-1").addClass('sel-active');
+                console.log($("#sel-1").offset());
                 $("#selector").animate({scrollLeft: $("#sel-1").offset().left}, "fast");
                 settings.onchange();
-            }
         });
-            
-        //set the default visibilities
-        if(select.val() == $("#sel-1").text()) { 
-            $("#sel-1").click();
-        } else {
-            $("#sel-0").click();
-        }
 
         //setup the view and show what needs to be shown
         isel.css('width', total_width);
         sel.css('height', $("sel-opt").css('font-size'));
         sel.css('width', max_width + max_width/parseInt(settings['peeking']));
         sel.css('overflow', 'hidden');
+        
+        //set the default visibilities
+        if(select.val() == $("#sel-1").text()) { 
+            $("#sel-1").click();
+        } else {
+            $("#sel-0").click();
+        }
     };
 })(jQuery);
