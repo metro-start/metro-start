cd07ad4521227fcea7be4525fea74289d0a10947
Chuma Nnaji <cnnaji2003@gmail.com>
2012-01-29 13:35:39 -0800

    Refactored

--- js/script.js
+++ js/script.js
@@ -1,28 +1,24 @@
 //globals (kinda?)
 var total = ['links', 'apps', 'bookmarks'];
 var units = ['fahrenheit', 'celsius'];
+
 $(function() {	
     //get links from localStorage and ensure they're empty	
-    var list = new Array();
     var links = localStorage.getItem("links");
-    if(links) {
-        links = JSON.parse(links);
-    }
-    else {
-        links = new Array();
-    }
+    if(links) links = JSON.parse(links);
+    else links = new Array();
+
 	if(!localStorage.getItem('hide_weather')) {
 		localStorage.setItem('hide_weather', false);
 	}
 
 	if(localStorage.getItem('active')) {
-		if(localStorage.getItem('active')) {
-			$('#menu').prop('selectedIndex', localStorage.getItem('active')); //saved links
-		} else {
-			localStorage.setItem('active', 1);
-		}
+		$('#menu').prop('selectedIndex', localStorage.getItem('active')); //saved links
+	} else {
+		localStorage.setItem('active', 1);
 	}
 	//load saved links or load default material
+    var list = new Array();
 	if(links == null || links.length == 0) {
 		addItem("use the wrench to get started. . . ", "");
 		list.push({"name": "use the wrench to get started. . . ", "url": ""});
@@ -32,7 +28,6 @@ $(function() {
 	for (id in links) {
 		addItem(links[id].name, links[id].url);
 	}
-
 
 	if(links != null && links.length >= 7) {
 		$(".add").hide();
@@ -52,9 +47,7 @@ $(function() {
 	}
 	$("#select-box").val(localStorage.getItem("unit"));
 
-	/** 
-	  Attaching event handlers
-	 */
+	//Attaching event handlers
 	//show all options on the page.
 	$("#wrench").click(function(){		
 		_gaq.push(['_trackEvent', 'wrench clicked']);
@@ -86,167 +79,167 @@ $(function() {
 		$("#zip").focus();
 	});
 
-//attach a picker for the background color and also set its default if it hasn't been changed yet
-$("#picker-background").farbtastic(function(color) {
-    localStorage.setItem("background-color", color);
-    updateStyle();
-});
-if(localStorage.getItem("background-color")) {
-    $.farbtastic("#picker-background").setColor(localStorage.getItem("background-color"));
-} else {
-    $.farbtastic("#picker-background").setColor("#000000")
-}
+	//attach a picker for the background color and also set its default if it hasn't been changed yet
+	$("#picker-background").farbtastic(function(color) {
+		localStorage.setItem("background-color", color);
+		updateStyle();
+	});
+	if(localStorage.getItem("background-color")) {
+		$.farbtastic("#picker-background").setColor(localStorage.getItem("background-color"));
+	} else {
+		$.farbtastic("#picker-background").setColor("#000000")
+	}
 
-//attach a picker for the title color and also set its default if it hasn't been changed yet
-$("#picker-title").farbtastic(function(color) {
-    localStorage.setItem("title-color", color);
-    updateStyle();
-});
-if(localStorage.getItem("title-color")) {
-    $.farbtastic("#picker-title").setColor(localStorage.getItem("title-color"));
-} else {
-    $.farbtastic("#picker-title").setColor("#4A4A4A")
-}
+	//attach a picker for the title color and also set its default if it hasn't been changed yet
+	$("#picker-title").farbtastic(function(color) {
+		localStorage.setItem("title-color", color);
+		updateStyle();
+	});
+	if(localStorage.getItem("title-color")) {
+		$.farbtastic("#picker-title").setColor(localStorage.getItem("title-color"));
+	} else {
+		$.farbtastic("#picker-title").setColor("#4A4A4A")
+	}
 
-//attach a picker for the main color and also set its default if it hasn't been changed yet
-$("#picker-main").farbtastic(function(color) {
-    localStorage.setItem("main-color", color);
-    updateStyle();
-});
-if(localStorage.getItem("main-color")) {
-    $.farbtastic("#picker-main").setColor(localStorage.getItem("main-color"));
-} else {
-    $.farbtastic("#picker-main").setColor("#FFFFFF")
-}
+	//attach a picker for the main color and also set its default if it hasn't been changed yet
+	$("#picker-main").farbtastic(function(color) {
+		localStorage.setItem("main-color", color);
+		updateStyle();
+	});
+	if(localStorage.getItem("main-color")) {
+		$.farbtastic("#picker-main").setColor(localStorage.getItem("main-color"));
+	} else {
+		$.farbtastic("#picker-main").setColor("#FFFFFF")
+	}
 
-//attach a picker for the options color and also set its default if it hasn't been changed yet
-$("#picker-options").farbtastic(function(color) {
-    localStorage.setItem("options-color", color);
-    updateStyle();
-});
-if(localStorage.getItem("options-color")) {
-    $.farbtastic("#picker-options").setColor(localStorage.getItem("options-color"));
-} else {
-    $.farbtastic("#picker-options").setColor("#FF0000");
-}
+	//attach a picker for the options color and also set its default if it hasn't been changed yet
+	$("#picker-options").farbtastic(function(color) {
+		localStorage.setItem("options-color", color);
+		updateStyle();
+	});
+	if(localStorage.getItem("options-color")) {
+		$.farbtastic("#picker-options").setColor(localStorage.getItem("options-color"));
+	} else {
+		$.farbtastic("#picker-options").setColor("#FF0000");
+	}
 
-//show the color pickers
-$("#colors").click(function(){
-    _gaq.push(['_trackEvent', 'colors clicked']);
-    $(".picker").toggle("fast");
-    $("#reset").toggle("fast");
-});
+	//show the color pickers
+	$("#colors").click(function(){
+		_gaq.push(['_trackEvent', 'colors clicked']);
+		$(".picker").toggle("fast");
+		$("#reset").toggle("fast");
+	});
 
-//reset all the colors to default.
-$("#reset").click(function(){
-    _gaq.push(['_trackEvent', 'colors reset']);
+	//reset all the colors to default.
+	$("#reset").click(function(){
+		_gaq.push(['_trackEvent', 'colors reset']);
 
-    localStorage.setItem("options-color", "#FF0000");	
-    $.farbtastic("#picker-options").setColor("#FF0000");
+		localStorage.setItem("options-color", "#FF0000");	
+		$.farbtastic("#picker-options").setColor("#FF0000");
 
-    localStorage.setItem("main-color", "#FFFFFF");	
-    $.farbtastic("#picker-main").setColor("#FFFFFF");
+		localStorage.setItem("main-color", "#FFFFFF");	
+		$.farbtastic("#picker-main").setColor("#FFFFFF");
 
-    localStorage.setItem("title-color", "#4A4A4A");	
-    $.farbtastic("#picker-title").setColor("#4A4A4A");
+		localStorage.setItem("title-color", "#4A4A4A");	
+		$.farbtastic("#picker-title").setColor("#4A4A4A");
 
-    localStorage.setItem("background-color", "#000000");	
-    $.farbtastic("#picker-background").setColor("#000000");
+		localStorage.setItem("background-color", "#000000");	
+		$.farbtastic("#picker-background").setColor("#000000");
 
-    updateStyle();
-});
+		updateStyle();
+	});
 
-//when you leave the url field
-$("#url").blur(function() {
-    $("#add").show();
-    $("#url").hide();
-});
+	//when you leave the url field
+	$("#url").blur(function() {
+		$("#add").show();
+		$("#url").hide();
+	});
 
-//when you leave the zip field
-$("#zip").blur(function() {
-    $("#edit").show();
-    $("#where").show();
-    $("#zip").hide();
-});
+	//when you leave the zip field
+	$("#zip").blur(function() {
+		$("#edit").show();
+		$("#where").show();
+		$("#zip").hide();
+	});
 
-//validate the url when the person hits enter
-$("#url").keyup(function(event){
-    if(event.keyCode == 13){
-        var list = JSON.parse(localStorage.getItem("links"));
-        var name = $("#url").val().toLowerCase().replace(/^https?\:\/\//i, "").replace(/^www\./i, "");
-        var url = $("#url").val();
-        if(url.trim() == "") {
-            return;
-        }
-        if(!url.match(/^([a-z]([a-z]|\d|\+|-|\.)*):(\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?((\[(|(v[\da-f]{1,}\.(([a-z]|\d|-|\.|_|~)|[!\$&'\(\)\*\+,;=]|:)+))\])|((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=])*)(:\d*)?)(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*|(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)|((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)|((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)){0})(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i)) {
-            //handle invalid url
-        }			
-        if(!url.match(/https?\:\/\//)) {
-    url = "http://" + url;
-    }
-    if (list == null) {
-        list = new Array();
-    }
-    list.push({"name": name, "url": url});
-    localStorage.setItem("links", JSON.stringify(list));
+	//validate the url when the person hits enter
+	$("#url").keyup(function(event){
+		if(event.keyCode == 13){
+			var list = JSON.parse(localStorage.getItem("links"));
+			var name = $("#url").val().toLowerCase().replace(/^https?\:\/\//i, "").replace(/^www\./i, "");
+			var url = $("#url").val();
+			if(url.trim() == "") {
+				return;
+			}
+			if(!url.match(/^([a-z]([a-z]|\d|\+|-|\.)*):(\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?((\[(|(v[\da-f]{1,}\.(([a-z]|\d|-|\.|_|~)|[!\$&'\(\)\*\+,;=]|:)+))\])|((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=])*)(:\d*)?)(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*|(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)|((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)|((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)){0})(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i)) {
+				//handle invalid url
+			}			
+			if(!url.match(/https?\:\/\//)) {
+		url = "http://" + url;
+		}
+		if (list == null) {
+			list = new Array();
+		}
+		list.push({"name": name, "url": url});
+		localStorage.setItem("links", JSON.stringify(list));
 
-    $("#url").blur();
-    addItem(name, url);
-    if(list.length >= 7) $(".add").hide();
-    }
-});
+		$("#url").blur();
+		addItem(name, url);
+		if(list.length >= 7) $(".add").hide();
+		}
+	});
 
-//force update the wheather when they hit enter after zip
-$("#zip").keyup(function(event){
-    if(event.keyCode == 13){
-        var zip = $("#zip").val();
-        if(zip.trim() == "") {
-            return;
-        }
-        localStorage.setItem("zip", zip);
-        $("#zip").blur();
-        updateWeather(true);
-    }
-});
+	//force update the wheather when they hit enter after zip
+	$("#zip").keyup(function(event){
+		if(event.keyCode == 13){
+			var zip = $("#zip").val();
+			if(zip.trim() == "") {
+				return;
+			}
+			localStorage.setItem("zip", zip);
+			$("#zip").blur();
+			updateWeather(true);
+		}
+	});
 
-//attaches a remove link to all <li>
-$("#links").delegate(".remove", "click", function(event){
-    var links = JSON.parse(localStorage.getItem("links"));
-    for(id in links) {
-        if(links[id].url == $(event.target).parent("li").children("a").attr("href")) {
-            _gaq.push(['_trackEvent', 'removed a link']);
-            links.splice(id, 1);
-            localStorage.setItem("links", JSON.stringify(links));
-            $(event.target).parent("li").remove();
-            break;
-        }
-    }
-});
+	//attaches a remove link to all <li>
+	$("#links").delegate(".remove", "click", function(event){
+		var links = JSON.parse(localStorage.getItem("links"));
+		for(id in links) {
+			if(links[id].url == $(event.target).parent("li").children("a").attr("href")) {
+				_gaq.push(['_trackEvent', 'removed a link']);
+				links.splice(id, 1);
+				localStorage.setItem("links", JSON.stringify(links));
+				$(event.target).parent("li").remove();
+				break;
+			}
+		}
+	});
 
-//attach the menu selectbox
-$("#menu").metroSelect({
-    'onchange': function() {
-        changeView($('#menu').attr('selectedIndex'));
-    }
-});
+	//attach the menu selectbox
+	$("#menu").metroSelect({
+		'onchange': function() {
+			changeView($('#menu').attr('selectedIndex'));
+		}
+	});
 
-//attach the weather selectbox
-$("#select-box").metroSelect({
-    'onchange': function() {
-        localStorage.setItem("unit", units[$("#select-box").attr('selectedIndex')]);
-        updateWeather(true);
-    }
-});
+	//attach the weather selectbox
+	$("#select-box").metroSelect({
+		'onchange': function() {
+			localStorage.setItem("unit", units[$("#select-box").attr('selectedIndex')]);
+			updateWeather(true);
+		}
+	});
 
-$(".option").hide();
-$("input").hide();
-$(".picker").hide();
+	$(".option").hide();
+	$("input").hide();
+	$(".picker").hide();
 
-//show the right page and load list of apps on load
-loadApps();
-loadBookmarks();
-updateWeather(false);
-updateStyle();	
+	//show the right page and load list of apps on load
+	loadApps();
+	loadBookmarks();
+	updateWeather(false);
+	updateStyle();	
 });
 
 function addItem(name, url) {
@@ -267,19 +260,19 @@ function updateWeather(force) {
         //delay for an hour
         localStorage.setItem("time", cTime.getTime() + 3600000);
         $.get(
-                "http://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20weather.bylocation%20WHERE%20location%3D'" + zip + "'%20AND%20unit%3D%22" + unit + "%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",
-                function(data) {
-                    var result = data.query.results.weather.rss.channel;
-                    var city = result.location.city.toLowerCase() + ", "+ result.location.region.toLowerCase();
-                    localStorage.setItem("where", city);
-                    $("#where").html(city);
-                    localStorage.setItem("temp", result.item.condition.temp + "<span class='option-color'>&deg;" + unit + "</span> / " + result.item.forecast[0].high + " <span class='option-color'>hi</span> / " + result.item.forecast[0].low + " <span class='option-color'>lo</span>");
-                    $("#temp").html(result.item.condition.temp  + "<span class='option-color'>&deg;" + unit + "</span> / " + result.item.forecast[0].high + " <span class='option-color'>hi</span> / " + result.item.forecast[0].low + " <span class='option-color'>lo</span>");
-                    localStorage.setItem("condition", result.item.condition.text.toLowerCase());
-                    $("#condition").html(result.item.condition.text.toLowerCase());
-                }
-             );
-    }
+				"http://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20weather.bylocation%20WHERE%20location%3D'" + zip + "'%20AND%20unit%3D%22" + unit + "%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",
+				function(data) {
+					var result = data.query.results.weather.rss.channel;
+					var city = result.location.city.toLowerCase() + ", "+ result.location.region.toLowerCase();
+					localStorage.setItem("where", city);
+					$("#where").html(city);
+					localStorage.setItem("temp", result.item.condition.temp + "<span class='option-color'>&deg;" + unit + "</span> / " + result.item.forecast[0].high + " <span class='option-color'>hi</span> / " + result.item.forecast[0].low + " <span class='option-color'>lo</span>");
+					$("#temp").html(result.item.condition.temp  + "<span class='option-color'>&deg;" + unit + "</span> / " + result.item.forecast[0].high + " <span class='option-color'>hi</span> / " + result.item.forecast[0].low + " <span class='option-color'>lo</span>");
+					localStorage.setItem("condition", result.item.condition.text.toLowerCase());
+					$("#condition").html(result.item.condition.text.toLowerCase());
+				}
+			 );
+	}
 }
 
 /**
@@ -410,15 +403,16 @@ var buildListOfBookmarks = function(owner, node) {
 function changeView(tar, instant) {
 	var cur = localStorage.getItem('active');
     localStorage.setItem('active', tar);
+	//If the page should be switched instantly, do not set change time
 	if(instant) {
 		for(i in total) {
 			if(i == tar) {
 			    $("." + total[i]).show();
 			   } else {
 			    $("." + total[i]).hide();
-   	//		    else $("." + total[i]).hide("fast");
 			}
 		}
+	//if the page is changing slowly, use a slide and 'fast' timer.
 	} else {
 		var direction = parseInt(cur) - parseInt(tar) > 0 ? 'left' : 'right';
 		var oppose = direction == 'left' ? 'right' : 'left';
