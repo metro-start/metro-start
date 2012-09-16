993d4f4932d1af7cce88f82361f3da1106f58a70
Chuma Nnaji <cnnaji2003@gmail.com>
2012-09-16 09:46:44 -0700

    Fixed google analytics.

--- js/analytics.js
+++ js/analytics.js
@@ -1,17 +1,5 @@
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-25604585-1']);
-_gaq.push(['_trackPageview']);
 (function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = 'https://ssl.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-25604585-1']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = 'https://ssl.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
