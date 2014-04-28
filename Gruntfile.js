53c52fcfc80845c211ab2f84d0e7754a72f79a96
Chuma Nnaji <cnnaji2003@gmail.com>
2014-04-27 18:42:52 -0700

    JSHint fixes and remove options

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,19 +4,16 @@ module.exports = function (grunt) {
   "use strict";
 
   grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json')
-    , jshint: {
+      pkg: grunt.file.readJSON('package.json'),
+      jshint: {
         all: [
           "js/*.js"
         ]
-      , options: {
-          jshintrc: '.jshintrc'
-        },
       }
-  })
+  });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint')
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
-  grunt.registerTask('test', ['jshint'])
-  grunt.registerTask('default', ['test'])
+  grunt.registerTask('test', ['jshint']);
+  grunt.registerTask('default', ['test']);
 };
