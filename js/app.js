c16cfd7be1b95dd04f7979d6858bf1f843703b50
Chuma Nnaji <cnnaji2003@gmail.com>
2014-05-11 18:20:44 -0700

    Add app.'s file

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-25604585-1']);
+_gaq.push(['_trackPageview']);
+
+define(['angular'],
+    function (angular, controllers) {
+        'use strict';
+
+        return angular.module('app', []);
+    });
