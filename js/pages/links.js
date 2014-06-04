298e24283ee3a20fa35ca8dc729d3905f6b12001
Chuma Nnaji <cnnaji2003@gmail.com>
2014-06-04 09:14:42 -0700

    Fix add/edit links

--- js/pages/links.js
+++ js/pages/links.js
@@ -8,83 +8,78 @@ define(['pages/base','utils/storage', 'utils/util'], function(base, storage, uti
 
         init: function(document, sort, pageItemCount) {
             this.elems.rootDom = document.getElementById('internal_selector_links');
+            this.elems.newUrlTitle = document.getElementById('newUrlTitle');
+            this.elems.newUrl = document.getElementById('newUrl');
+            this.elems.addLink = document.getElementById('addLink');
+            this.elems.addLink.addEventListener('submit', this.addLink.bind(this));
+
             this.loadLinks(sort, pageItemCount);
         },
 
         setPageItemCount: function(pageItemCount) {
-            this.links.setPageItemCount(pageItemCount, this.data);
+            this.links.setPageItemCount(pageItemCount - 1, this.data); //-1 to account for form
         },
 
         loadLinks: function(sort, pageItemCount) {
-//            this.data = storage.get('links', [{'name': 'use the wrench to get started. . . ', 'url': ''}]);
-            this.data = [
-{'name': 'wrench ', 'url': ''},
-{'name': 'wrench ', 'url': ''},
-{'name': 'wrench ', 'url': ''},
-{'name': 'wrench ', 'url': ''},
-{'name': 'wrench ', 'url': ''},
-{'name': 'wrench ', 'url': ''},
-{'name': 'wrench ', 'url': ''},
-{'name': 'wrench ', 'url': ''},
-{'name': 'wrench ', 'url': ''},
-{'name': 'wrench ', 'url': ''},
-{'name': 'wrench ', 'url': ''},
-{'name': 'wrench ', 'url': ''},
-            ];
+            this.data = storage.get('links', [{'name': 'use the wrench to get started. . . ', 'url': ''}]);
             this.links = new base(this.elems.rootDom, this.data, sort, pageItemCount, this.callback.bind(this));
             this.links.buildDom();
         },
 
-        template: {
+        templates: {
             linkFragment: util.createElement('<a class="title"></a>'),
             removeFragment: util.createElement('<span class="remove option options-color small-text clickable">remove</span>'),
             editFragment: util.createElement('<span class="remove option options-color small-text clickable">edit</span>'),
         },
 
         callback: function(elem, item) {
-            var link = this.template.linkFragment.cloneNode(true);
+            var link = this.templates.linkFragment.cloneNode(true);
             link.firstChild.href = item.url;
             link.firstChild.textContent = item.name;
             elem.appendChild(link);
 
-            var remove = this.template.removeFragment.cloneNode(true);
+            var remove = this.templates.removeFragment.cloneNode(true);
             remove.firstChild.addEventListener('click', this.removeLink.bind(this, item));
             elem.appendChild(remove);
 
-            var edit = this.template.editFragment.cloneNode(true);
+            var edit = this.templates.editFragment.cloneNode(true);
             edit.firstChild.addEventListener('click', this.editLink.bind(this, item));
             elem.appendChild(edit);
         },
 
-        addLink: function() {
-            if(!$scope.newUrl.match(/https?\:\/\//)) {
-                $scope.newUrl = 'http://' + $scope.newUrl;
-            }
-            if (!$.isEmptyObject($scope.linkToEdit)) {
-                $scope.linkToEdit.name =  $scope.newUrlTitle ? $scope.newUrlTitle : $scope.newUrl.toLocaleLowerCase().replace(/^https?\:\/\//i, '').replace(/^www\./i, '');
-                $scope.linkToEdit.url = $scope.newUrl;
-                $scope.linkToEdit = {};
+        addLink: function(event) {
+            var newUrlTitle = this.elems.newUrlTitle.value.trim();
+            var newUrl = this.elems.newUrl.value.trim();
+            if (newUrl !== '') {
+                if (!newUrl.match(/https?\:\/\//)) {
+                    newUrl = 'http://' + newUrl;
+                }
+                var formatTitle = function(title) {
+                    return newUrlTitle ? newUrlTitle : newUrl.toLocaleLowerCase().replace(/^https?\:\/\//i, '').replace(/^www\./i, '');
+                };
+                if (this.linkToEdit) {
+                    this.linkToEdit.name = formatTitle(newUrlTitle);
+                    this.linkToEdit.url = newUrl;
 
-                _gaq.push(['_trackEvent', 'Links', 'Save Edited Link']);
-            } else {
-                $scope.links.add({
-                    'name': $scope.newUrlTitle ? $scope.newUrlTitle : $scope.newUrl.toLocaleLowerCase().replace(/^https?\:\/\//i, '').replace(/^www\./i, ''),
-                    'url': $scope.newUrl,
-                });
+                    _gaq.push(['_trackEvent', 'Links', 'Finished Editing Link']);
+                } else {
+                    this.data.push({
+                        'name': formatTitle(newUrlTitle),
+                        'url': newUrl
+                    });
 
-                _gaq.push(['_trackEvent', 'Links', 'Add New Link']);
+                    _gaq.push(['_trackEvent', 'Links', 'Add New Link']);
+                }
+                storage.save('links', this.data);
+                this.links.relayout(this.data);
             }
-            $scope.newUrl = '';
-            $scope.newUrlTitle = '';
-            storage.save('links', $scope.links.flatten());
+            event.preventDefault();
         },
 
         editLink: function(link) {
-            console.log(this);
-            console.log(link);
-            $scope.linkToEdit = $scope.links.get(page, index);
-            $scope.newUrlTitle = $scope.linkToEdit.name;
-            $scope.newUrl = $scope.linkToEdit.url;
+            this.linkToEdit = link;
+            this.elems.newUrlTitle.value = link.name;
+            this.elems.newUrl.value = link.url;
             _gaq.push(['_trackEvent', 'Links', 'Start Editing Link']);
         },
 
