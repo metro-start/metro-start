485755d7be99f5297012fe82165e60d00dcea363
Chuma Nnaji <cnnaji2003@gmail.com>
2014-06-23 15:12:07 -0700

    fixed bookmarks

--- js/pages/bookmarks.js
+++ js/pages/bookmarks.js
@@ -1,81 +1,81 @@
-define([], function() {
+define(['pages/pagebase', 'utils/util'], function(pagebase, util) {
     var bookmarks = {
+        name: 'bookmarks',
+
         data: {},
 
-        init: function() {
+        elems: {},
 
+        bookmarks: {},
+
+        templates: {
+            titleFragment: util.createElement('<a class="bookmark_item"></a>'),
+            removeFragment: util.createElement('<span class="remove option options-color small-text clickable" ng-click="removeBookmark(bookmark, $parent.$index, $index)">remove</span>'),
+            slashFragment: util.createElement('<span class="options-color">/</span>'),
         },
 
-        setPageItemCount: function(pageItemCount) {
-            // this.links.setPageItemCount(pageItemCount);
+        init: function() {
+            this.elems.rootNode = document.getElementById('internal_selector_bookmarks');
+            this.bookmarks = new pagebase(document, this.name, this.elems.rootNode, this.templateFunc.bind(this));
+            this.bookmarks.pageItemCount = -1;
+            this.loadBookmarks();
         },
 
         loadBookmarks: function() {
+            var that = this;
             chrome.bookmarks.getTree(function(data) {
-                $scope.$apply(function() {
-                    $scope.bookmarks = [data[0].children];
-                });
+                that.data = data[0].children;
+                that.bookmarks.addAll(that.data);
             });
         },
 
-        /**
-            Click event handler for bookmarks.
-            Allows me to capture folder clicks.
-            bookmark: The bookmark that was clicked.
-            pageIndex: The page that contains the bookmark.
-        */
-        clickBookmark: function(bookmark, page) {
-            if (bookmark.children.length > 0) {
-                // Deactiviate bookmark and its siblings.
-                siblings = $scope.bookmarks[page];
-                for(i = 0; i < siblings.length; i++) {
-                    siblings[i].active = false;
-                }
-
-                // Activate bookmark after it and it's siblings are deactiveated.
-                bookmark.active = true;
-
-                // Deactive children.
-                for(i = 0; i < bookmark.children.length; i++) {
-                    bookmark.children[i].active = false;
-                }
-
-                $scope.bookmarks.length = page + 1; // Remove all pages ahead of this.
-                if ($scope.sort.bookmarks) {
-                    $scope.bookmarks.push(bookmark.children.sort(function(a, b) {
-                        if (a.title.toLocaleLowerCase() > b.title.toLocaleLowerCase()) {
-                            return 1;
-                        } else if(a.title.toLocaleLowerCase() < b.title.toLocaleLowerCase()) {
-                            return -1;
-                        } else {
-                            return 0;
-                        }
-                    }));
-                } else {
-                    $scope.bookmarks.push(bookmark.children);
-                }
-                _gaq.push(['_trackEvent', 'Bookmarks', 'Bookmarked Folder']);
-                return false;
+        clickBookmark: function(bookmark, bookmarkNode, event) {
+            if (bookmark.children && bookmark.children.length > 0) {
+                this.activateBookmark(bookmarkNode);
+                var currentPage = bookmarkNode.parentNode.parentNode.id;
+                this.bookmarks.truncatePages(currentPage.replace('bookmarks_', ''));
+                this.bookmarks.addAll(bookmark.children);
+                event.preventDefault();
+                _gaq.push(['_trackEvent', 'Bookmarks', 'Click Bookmark Folder']);
             }
-            _gaq.push(['_trackEvent', 'Bookmarks', 'Bookmarked Page']);
+            _gaq.push(['_trackEvent', 'Bookmarks', 'Click Bookmarked Page']);
         },
 
-        /**
-            Remove the given bookmark.
-            bookmark: The bookmark to be removed.
-            page: The page where the bookmark is.
-            index: The index for the bookmark.
-        */
         removeBookmark: function(bookmark, page, index) {
             chrome.bookmarks.removeTree(bookmark.id, function() {
                 $scope.$apply(function() {
                     $scope.bookmarks[page].splice(index, 1);
                 });
             });
-
             _gaq.push(['_trackEvent', 'Bookmarks', 'Remove Bookmarked']);
+        },
+
+        setShowOptions: function setShowOptions(showOptions) {
+            this.bookmarks.setShowOptions(showOptions);
+        },
+
+        activateBookmark: function activateBookmark(bookmarkElem) {
+            // bookmarkEle
+        },
+
+        templateFunc: function(bookmark) {
+            var fragment = util.createElement('');
+            var title = this.templates.titleFragment.cloneNode(true);
+            title.firstElementChild.href = bookmark.url;
+            title.firstElementChild.textContent = bookmark.title;
+            title.firstElementChild.id = 'bookmark_' + bookmark.id;
+            if (bookmark.children && bookmark.children.length > 0) {
+                title.firstElementChild.appendChild(this.templates.slashFragment.cloneNode(true));
+            }
+            title.firstElementChild.addEventListener('click', this.clickBookmark.bind(this, bookmark, title.firstElementChild));
+            fragment.appendChild(title);
+
+            var remove = this.templates.removeFragment.cloneNode(true);
+            remove.firstElementChild.addEventListener('click', this.removeBookmark.bind(this, bookmark));
+            fragment.appendChild(remove);
+
+            return fragment;
         }
     };
-
     return bookmarks;
 });
--- js/pages/pagebase.js
+++ js/pages/pagebase.js
@@ -9,7 +9,9 @@ define(['utils/util', 'utils/storage', 'metro-select'], function(util, storage, 
         this.name = name;
         this.rootNode = rootNode;
         this.sort = storage.get(this.name + '_sort', false);
+        this.currentPage = 0;
         this.templateFunc = templateFunc;
+        this.page = 0;
 
         this.init(document);
     };
@@ -21,6 +23,8 @@ define(['utils/util', 'utils/storage', 'metro-select'], function(util, storage, 
         selector.metroSelect({
             'onchange': this.sortChanged.bind(this)
         });
+
+        this.elems.internal_selector = document.getElementById('internal_selector_' + this.name);
     };
 
     pagebase.prototype.sortChanged = function sortChagned(sort) {
@@ -36,6 +40,8 @@ define(['utils/util', 'utils/storage', 'metro-select'], function(util, storage, 
 
     pagebase.prototype.rebuildDom = function() {
         var nodes = [];
+        this.currentPage = 0;
+
         while (this.rootNode.firstElementChild) {
             var column = this.rootNode.firstElementChild;
             while (column.firstElementChild) {
@@ -48,18 +54,27 @@ define(['utils/util', 'utils/storage', 'metro-select'], function(util, storage, 
     };
 
     pagebase.prototype.buildDom = function buildDom(rows) {
+        this.currentPage = 0;
         while (this.rootNode.firstElementChild) {
             this.rootNode.firstElementChild.remove();
         }
+        this.addAll(rows);
+    };
+
+    pagebase.prototype.addAll = function addAll(rows) {
         var nodes = [];
         for (var i = 0; i < rows.length; i++) {
             var item = templates.item.cloneNode(true);
             item.id = this.name + '_' + i;
             item.firstElementChild.id = this.name + '_' + i;
-            item.firstElementChild.appendChild(this.templateFunc(rows[i]));
+            item.firstElementChild.appendChild(this.templateFunc(rows[i], this.currentPage));
             nodes.push(item);
         }
         this.addAllNodes(nodes);
+    };
+
+    pagebase.prototype.getPages = function getPages() {
+        return Array.prototype.slice.call(this.elems.internal_selector.children);
     };
 
     pagebase.prototype.addAllNodes = function addAllNodes(nodes) {
@@ -71,7 +86,10 @@ define(['utils/util', 'utils/storage', 'metro-select'], function(util, storage, 
             });
         }
         if (nodes.length) {
+            // this.page = 0;
+            var pageIndex = this.elems.internal_selector.children.length;
             var columnNode = templates.column.cloneNode(true);
+            columnNode.firstElementChild.id = this.name + '_' + pageIndex;
             var pageItemCount = this.pageItemCount;
             if (this.showOptions) {
                 pageItemCount--; // If the options are showing, account for sort options.
@@ -81,13 +99,14 @@ define(['utils/util', 'utils/storage', 'metro-select'], function(util, storage, 
             }
             //Add each row to an column and create new ones on the pageItemCount boundary.
             for (var i = 0; i < nodes.length; i++) {
-                if (i !== 0 && i % pageItemCount === 0) { //Skip the first row.
+                if (i !== 0 && i % pageItemCount === 0 && pageItemCount > 0) { //Skip the first row.
                     this.rootNode.appendChild(columnNode);
                     columnNode = templates.column.cloneNode(true);
+                    columnNode.firstElementChild.id = this.name + '_' + pageIndex++;
                 }
                 columnNode.firstElementChild.appendChild(nodes[i]);
             }
-            if ((i - 1) % this.pageItemCount !== 0) { // - 1 to account for the for loop going one past last good index.
+            if ((i - 1) % this.pageItemCount !== 0 || this.pageItemCount === -1) { // - 1 to account for the for loop going one past last good index.
                 this.rootNode.appendChild(columnNode);
             }
         }
@@ -103,6 +122,16 @@ define(['utils/util', 'utils/storage', 'metro-select'], function(util, storage, 
     pagebase.prototype.setShowOptions = function setShowOptions(showOptions) {
         this.showOptions = showOptions;
         this.rebuildDom();
+    };
+
+    pagebase.prototype.truncatePages = function truncatePages(pageNumber) {
+        // var page_number = this.parentNode.id.remove('pages_');
+        var nodes = Array.prototype.slice.call(this.elems.internal_selector.children);
+        console.log(parseInt(pageNumber) + 1);
+        nodes.splice(0, parseInt(pageNumber) + 1);
+        nodes.forEach(function(node) {
+            node.remove();
+        });
     };
 
     return pagebase;
