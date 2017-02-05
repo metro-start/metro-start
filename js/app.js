f17baa3425932cb5be599ff0b2662a332ec692d3
Chuma Nnaji <cnnaji2003@gmail.com>
2017-02-05 11:35:42 -0800

    Clean dev

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
-define(['detect-dom-ready', './pages/pages', './widgets/widgets', './utils/defaults', './utils/script', './utils/storage', './utils/util'],
-  function (domready, pages, widgets, defaults, script, storage, util) {
+define(['detect-dom-ready', './pages/pages', './widgets/widgets', './utils/defaults', './utils/script', './utils/storage'],
+  function (domready, pages, widgets, defaults, script, storage) {
     'use strict';
 
-    var that = this;
     var app = {
       data: {},
 
@@ -24,9 +23,6 @@ define(['detect-dom-ready', './pages/pages', './widgets/widgets', './utils/defau
         });
 
         this.elems.hideRule = document.getElementById('hideRule');
-        // widgets.weather.updateWeather(false);
-        // script.updateStyle(defaults.defaultTheme, false);
-        // $scope.updateWeather(false);
       },
 
       clickWrench: function () {
@@ -55,52 +51,6 @@ define(['detect-dom-ready', './pages/pages', './widgets/widgets', './utils/defau
       setPageItemCount: function (pageItemCount) {
         pages.pageItemCount = pageItemCount;
         pages.setPageItemCount(pageItemCount);
-      },
-
-      changeSorting: function (key, newSorting) {
-        $scope.sort[key] = newSorting;
-        storage.save('sort', $scope.sort);
-        if ($scope.sort[key] === 0) {
-          if (key == 'links') {
-            $scope.loadLinks();
-          } else if (key == 'apps') {
-            $scope.loadApps();
-          } else if (key == 'themes') {
-            $scope.loadThemes();
-          } else if (key == 'bookmarks') {
-            var handleBookmarks = function (res) {
-              $scope.$apply(function () {
-                $scope.bookmarks[i] = res;
-              });
-            };
-            for (i = $scope.bookmarks.length - 1; i >= 0; i--) {
-              var parentId = typeof $scope.bookmarks[i][0].parentId;
-              if (typeof parentId !== 'undefined') {
-                chrome.bookmarks.getChildren(parentId, handleBookmarks);
-              }
-            }
-          }
-        } else if ($scope.sort[key] == 1) {
-          if (key == 'themes') {
-            $scope.localThemes.sort();
-            $scope.onlineThemes.sort();
-          } else if (key == 'bookmarks') {
-            var compareFunction = function (a, b) {
-              if (a.title.toLocaleLowerCase() > b.title.toLocaleLowerCase()) {
-                return 1;
-              } else if (a.title.toLocaleLowerCase() < b.title.toLocaleLowerCase()) {
-                return -1;
-              } else {
-                return 0;
-              }
-            };
-            for (i = 0; i < $scope.bookmarks.length; i++) {
-              $scope.bookmarks[i].sort(compareFunction);
-            }
-          } else {
-            $scope[key].sort();
-          }
-        }
       }
     };
 
