eac6f3a26b332430ded6d02efcfbcd3caa0189bf
Chuma Nnaji <cnnaji2003@gmail.com>
2017-02-22 07:33:42 -0800

    Revert "Cleanup animations"
    This reverts commit 9d98e4a8126d0e23de6030d45a152912a47d83fb.

--- js/utils/script.js
+++ js/utils/script.js
@@ -1,65 +1,91 @@
-define(['jquery', 'jquery-color', 'jss', './util', './storage'],
-  function (jquery, jqueryColor, jss, util, storage) {
-    var fonts = ['"Segoe UI", Helvetica, Arial, sans-serif', 'Raleway, "Segoe UI", Helvetica, Arial, sans-serif'];
+define(['jquery', 'jquery-color', 'jss', './util', './storage'], 
+function (jquery, jqueryColor, jss, util, storage) {
+  var fonts = ['"Segoe UI", Helvetica, Arial, sans-serif', 'Raleway, "Segoe UI", Helvetica, Arial, sans-serif'];
 
-    var script = {
-      init: function () { },
+  var script = {
+    init: function () { },
 
-      /**
-      Updates the page style with colors provided.
-      theme: The new theme to use.
-      transition: If true, animate the color transition.
-      */
-      updateStyle: function (theme, transition) {
-        this.updateFont();
+    /**
+    Changes the style to whatever is in the scope.
+    theme: The new theme to change to.
+    transition: A bool indicating whether to slowly transition or immediately change.
+    */
+    updateStyle: function (theme, transition) {
+      this.updateFont();
 
-        var options_color = theme.colors['options-color'];
-        var background_color = theme.colors['background-color'];
-        var main_color = theme.colors['main-color'];
-        var title_color = theme.colors['title-color'];
+      var options_color = theme.colors['options-color'];
+      var background_color = theme.colors['background-color'];
+      var main_color = theme.colors['main-color'];
+      var title_color = theme.colors['title-color'];
 
-        // Animate the color transition.
-        var duration = 1200;
-        if (transition === false) {
-          duration = 0;
-        }
+      jss.set('*', {
+        'border-color': options_color
+      });
 
-        jquery('*').animate({ 'borderColor': options_color }, { duration: duration, queue: false, easing: 'linear' });
-        jquery('*').animate({ 'color': main_color }, { duration: duration, queue: false, easing: 'linear' });
-        jquery('.background-color').animate({ 'backgroundColor': background_color }, { duration: duration, queue: false, easing: 'linear' });
-        jquery('.titles-color').animate({ 'color': title_color }, { duration: duration, queue: false, easing: 'linear' });
-        jquery('body').animate({ 'color': main_color }, { duration: duration, queue: false, easing: 'linear' });
-        jquery('input').animate({ 'color': main_color }, { duration: duration, queue: false, easing: 'linear' });
-        jquery('.options-color').animate({ 'color': options_color }, { duration: duration, queue: false, easing: 'linear' });
-        jquery('#picker').animate({ 'borderColor': options_color }, { duration: duration, queue: false, easing: 'linear' });
-        jquery('.overlay').animate({ 'backgroundColor': options_color }, { duration: duration, queue: false, easing: 'linear' });
-        jquery('.bookmark-active').animate({ 'backgroundColor': options_color }, { duration: duration, queue: false, easing: 'linear' });
+      jss.set('::-webkit-scrollbar', {
+        'background': background_color
+      });
 
-        // ...poor sods that can't be animated. SAD!
-        jss.set('::-webkit-scrollbar', {
-          'background': background_color
-        });
-        jss.set('::-webkit-scrollbar-thumb', {
-          'background': options_color
-        });
-        jss.set('::-webkit-input-placeholder', {
-          'background': main_color
-        });
-        jss.set('.pagebase-grouped > .group > .page', {
-          'border-top-style': 'solid',
-          'border-top-width': '1px'
-        });
-        jss.set('#picker', {
-          'border': '1px solid'
-        });
-      },
+      jss.set('::-webkit-scrollbar-thumb', {
+        'background': options_color
+      });
 
-      updateFont: function () {
-        jss.set('body', {
-          'font-family': fonts[storage.get('currentFont', 0)],
-        });
+      jss.set('::-webkit-input-placeholder', {
+        'background': main_color
+      });
+
+      // Animate the color transition.
+      if (transition) {
+        jquery('.background-color').animate({ 'backgroundColor': background_color }, { duration: 800, queue: false });
+        jquery('.titles-color').animate({ 'color': title_color }, { duration: 800, queue: false });
+        jquery('body').animate({ 'color': main_color }, { duration: 1200, queue: false });
+        jquery('input').animate({ 'color': main_color }, { duration: 1600, queue: false });
+        jquery('.options-color').animate({ 'color': options_color }, { duration: 1200, queue: false });
       }
-    };
 
-    return script;
-  });
+      // ...but then we still need to add it to the DOM.
+      jss.set('.background-color', {
+        'background-color': background_color
+      });
+      jss.set('.titles-color', {
+        'color': title_color
+      });
+      jss.set('body', {
+        'color': main_color
+      });
+      jss.set('input', {
+        'color': main_color
+      });
+      jss.set('.options-color', {
+        'color': options_color
+      });
+      jss.set('.bookmark-active', {
+        'background-color': options_color
+      });
+      jss.set('.overlay', {
+        'background-color': options_color
+      });
+      jss.set('.pagebase-grouped > .group > .page', {
+        'border-top-style': 'solid',
+        'border-top-width': '1px',
+        'border-top-color': options_color
+      });
+      jss.set('.pagebase-grouped > .group > .page', {
+        'border-top-style': 'solid',
+        'border-top-width': '1px',
+        'border-top-color': options_color
+      });
+      jss.set('#picker', {
+        'border': '1px solid ' + options_color
+      });
+    },
+
+    updateFont: function () {
+      jss.set('body', {
+        'font-family': fonts[storage.get('currentFont', 0)],
+      });
+    }
+  };
+
+  return script;
+});
